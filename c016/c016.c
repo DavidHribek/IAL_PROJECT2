984bfa8ba73ef29f8b200be68d946562127438a6
David Hříbek <scottik4@seznam.cz>
2017-11-12 22:41:12 +0100

    initial

---
+++ c016/c016.c
@@ -0,0 +1,201 @@
+
+/* c016.c: **********************************************************}
+{* Téma:  Tabulka s Rozptýlenými Položkami
+**                      První implementace: Petr Přikryl, prosinec 1994
+**                      Do jazyka C prepsal a upravil: Vaclav Topinka, 2005
+**                      Úpravy: Karel Masařík, říjen 2014
+**                              Radek Hranický, říjen 2014
+**                              Radek Hranický, listopad 2015
+**                              Radek Hranický, říjen 2016
+**
+** Vytvořete abstraktní datový typ
+** TRP (Tabulka s Rozptýlenými Položkami = Hash table)
+** s explicitně řetězenými synonymy. Tabulka je implementována polem
+** lineárních seznamů synonym.
+**
+** Implementujte následující procedury a funkce.
+**
+**  HTInit ....... inicializuje tabulku před prvním použitím
+**  HTInsert ..... vložení prvku
+**  HTSearch ..... zjištění přítomnosti prvku v tabulce
+**  HTDelete ..... zrušení prvku
+**  HTRead ....... přečtení hodnoty prvku
+**  HTClearAll ... zrušení obsahu celé tabulky (inicializace tabulky
+**                 poté, co již byla použita)
+**
+** Definici typů naleznete v souboru c016.h.
+**
+** Tabulka je reprezentována datovou strukturou typu tHTable,
+** která se skládá z ukazatelů na položky, jež obsahují složky
+** klíče 'key', obsahu 'data' (pro jednoduchost typu float), a
+** ukazatele na další synonymum 'ptrnext'. Při implementaci funkcí
+** uvažujte maximální rozměr pole HTSIZE.
+**
+** U všech procedur využívejte rozptylovou funkci hashCode.  Povšimněte si
+** způsobu předávání parametrů a zamyslete se nad tím, zda je možné parametry
+** předávat jiným způsobem (hodnotou/odkazem) a v případě, že jsou obě
+** možnosti funkčně přípustné, jaké jsou výhody či nevýhody toho či onoho
+** způsobu.
+**
+** V příkladech jsou použity položky, kde klíčem je řetězec, ke kterému
+** je přidán obsah - reálné číslo.
+*/
+
+#include "c016.h"
+
+int HTSIZE = MAX_HTSIZE;
+int solved;
+
+/*          -------
+** Rozptylovací funkce - jejím úkolem je zpracovat zadaný klíč a přidělit
+** mu index v rozmezí 0..HTSize-1.  V ideálním případě by mělo dojít
+** k rovnoměrnému rozptýlení těchto klíčů po celé tabulce.  V rámci
+** pokusů se můžete zamyslet nad kvalitou této funkce.  (Funkce nebyla
+** volena s ohledem na maximální kvalitu výsledku). }
+*/
+
+int hashCode ( tKey key ) {
+	int retval = 1;
+	int keylen = strlen(key);
+	for ( int i=0; i<keylen; i++ )
+		retval += key[i];
+	return ( retval % HTSIZE );
+}
+
+/*
+** Inicializace tabulky s explicitně zřetězenými synonymy.  Tato procedura
+** se volá pouze před prvním použitím tabulky.
+*/
+
+void htInit ( tHTable* ptrht ) {
+
+    if (ptrht) { // pokud je ukazatel na tabulku validni
+        for (int i = 0; i < HTSIZE; i++) {
+            (*ptrht)[i] = NULL;
+        }
+    }
+
+}
+
+/* TRP s explicitně zřetězenými synonymy.
+** Vyhledání prvku v TRP ptrht podle zadaného klíče key.  Pokud je
+** daný prvek nalezen, vrací se ukazatel na daný prvek. Pokud prvek nalezen není, 
+** vrací se hodnota NULL.
+**
+*/
+
+tHTItem* htSearch ( tHTable* ptrht, tKey key ) {
+
+    if (ptrht) { // pokud je ukazatel na tabulku validni
+        for (int i = 0; i < HTSIZE; i++) {
+            if ( (*ptrht)[i] != NULL ) { // pokud je na danem indexu ukazatel na synonymum
+                if ( (*ptrht)[i]->key == key ) {
+                    return (*ptrht)[i];
+                }
+            }
+        }
+    }
+    return NULL; // pokud prvek s klicem neni nalezen
+
+}
+
+/* 
+** TRP s explicitně zřetězenými synonymy.
+** Tato procedura vkládá do tabulky ptrht položku s klíčem key a s daty
+** data.  Protože jde o vyhledávací tabulku, nemůže být prvek se stejným
+** klíčem uložen v tabulce více než jedenkrát.  Pokud se vkládá prvek,
+** jehož klíč se již v tabulce nachází, aktualizujte jeho datovou část.
+**
+** Využijte dříve vytvořenou funkci htSearch.  Při vkládání nového
+** prvku do seznamu synonym použijte co nejefektivnější způsob,
+** tedy proveďte.vložení prvku na začátek seznamu.
+**/
+
+void htInsert ( tHTable* ptrht, tKey key, tData data ) {
+
+    if (ptrht) { // pokud je ukazatel na tabulku validni
+        tHTItem *newitem; // novy prvek
+        if ( (newitem = (tHTItem*)malloc(sizeof(tHTItem))) == NULL ) {
+            return; // chyba pri vytvareni noveho prvku
+        }
+        // inicializace noveho prvku
+        newitem->key = key;
+        newitem->data = data;
+        newitem->ptrnext = NULL;
+        // vlozeni prvku
+        tHTItem *item = htSearch(ptrht, key);
+        if (item) {
+            item->data = data;
+        }
+        else {
+            tHTItem *itemToJoin = (*ptrht)[hashCode(key)];
+            newitem->ptrnext = itemToJoin;
+            (*ptrht)[hashCode(key)] = newitem;
+        }
+    }
+
+}
+
+/*
+** TRP s explicitně zřetězenými synonymy.
+** Tato funkce zjišťuje hodnotu datové části položky zadané klíčem.
+** Pokud je položka nalezena, vrací funkce ukazatel na položku
+** Pokud položka nalezena nebyla, vrací se funkční hodnota NULL
+**
+** Využijte dříve vytvořenou funkci HTSearch.
+*/
+
+tData* htRead ( tHTable* ptrht, tKey key ) {
+
+    if (ptrht) { // pokud je ukazatel na tabulku validni
+        tHTItem *searchedItem = htSearch(ptrht, key);
+        if (searchedItem) { // pokud byla nalezena polozka s danym klicem
+            return &(searchedItem->data);
+        }
+    }
+    return NULL; // pokud polozka nebyla nalezena
+
+}
+
+/*
+** TRP s explicitně zřetězenými synonymy.
+** Tato procedura vyjme položku s klíčem key z tabulky
+** ptrht.  Uvolněnou položku korektně zrušte.  Pokud položka s uvedeným
+** klíčem neexistuje, dělejte, jako kdyby se nic nestalo (tj. nedělejte
+** nic).
+**
+** V tomto případě NEVYUŽÍVEJTE dříve vytvořenou funkci HTSearch.
+*/
+
+void htDelete ( tHTable* ptrht, tKey key ) {
+
+    if (ptrht) { // pokud ukazatel na tabulku je validni
+        int indexToDelete = hashCode(key);
+        tHTItem *itemToDelete = (*ptrht)[indexToDelete];
+        if (itemToDelete) { // pokud je co mazat
+            (*ptrht)[indexToDelete] = (*ptrht)[indexToDelete]->ptrnext;
+            free(itemToDelete);
+        }
+    }
+
+}
+
+/* TRP s explicitně zřetězenými synonymy.
+** Tato procedura zruší všechny položky tabulky, korektně uvolní prostor,
+** který tyto položky zabíraly, a uvede tabulku do počátečního stavu.
+*/
+
+void htClearAll ( tHTable* ptrht ) {
+
+    if (ptrht) { // pokud je ukazatel na tabulku validni
+        for (int i = 0; i < HTSIZE; i++) {
+            if ( (*ptrht)[i] ) { // pokud dany index tabulky neni prazdny
+                tKey k = (*ptrht)[i]->key;
+                while ( (*ptrht)[i] != NULL ) {
+                    htDelete(ptrht,k);
+                }
+            }
+        }
+    }
+
+}
---
+++ c401/c401.c
@@ -0,0 +1,226 @@
+
+/* c401.c: **********************************************************}
+{* Téma: Rekurzivní implementace operací nad BVS
+**                                         Vytvořil: Petr Přikryl, listopad 1994
+**                                         Úpravy: Andrea Němcová, prosinec 1995
+**                                                      Petr Přikryl, duben 1996
+**                                                   Petr Přikryl, listopad 1997
+**                                  Převod do jazyka C: Martin Tuček, říjen 2005
+**                                         Úpravy: Bohuslav Křena, listopad 2009
+**                                         Úpravy: Karel Masařík, říjen 2013
+**                                         Úpravy: Radek Hranický, říjen 2014
+**                                         Úpravy: Radek Hranický, listopad 2015
+**                                         Úpravy: Radek Hranický, říjen 2016
+**
+** Implementujte rekurzivním způsobem operace nad binárním vyhledávacím
+** stromem (BVS; v angličtině BST - Binary Search Tree).
+**
+** Klíčem uzlu stromu je jeden znak (obecně jím může být cokoliv, podle
+** čeho se vyhledává). Užitečným (vyhledávaným) obsahem je zde integer.
+** Uzly s menším klíčem leží vlevo, uzly s větším klíčem leží ve stromu
+** vpravo. Využijte dynamického přidělování paměti.
+** Rekurzivním způsobem implementujte následující funkce:
+**
+**   BSTInit ...... inicializace vyhledávacího stromu
+**   BSTSearch .... vyhledávání hodnoty uzlu zadaného klíčem
+**   BSTInsert .... vkládání nové hodnoty
+**   BSTDelete .... zrušení uzlu se zadaným klíčem
+**   BSTDispose ... zrušení celého stromu
+**
+** ADT BVS je reprezentován kořenovým ukazatelem stromu (typ tBSTNodePtr).
+** Uzel stromu (struktura typu tBSTNode) obsahuje klíč (typu char), podle
+** kterého se ve stromu vyhledává, vlastní obsah uzlu (pro jednoduchost
+** typu int) a ukazatel na levý a pravý podstrom (LPtr a RPtr). Přesnou definici typů 
+** naleznete v souboru c401.h.
+**
+** Pozor! Je třeba správně rozlišovat, kdy použít dereferenční operátor *
+** (typicky při modifikaci) a kdy budeme pracovat pouze se samotným ukazatelem 
+** (např. při vyhledávání). V tomto příkladu vám napoví prototypy funkcí.
+** Pokud pracujeme s ukazatelem na ukazatel, použijeme dereferenci.
+**/
+
+#include "c401.h"
+int solved;
+
+void BSTInit (tBSTNodePtr *RootPtr) {
+/*   -------
+** Funkce provede počáteční inicializaci stromu před jeho prvním použitím.
+**
+** Ověřit, zda byl již strom předaný přes RootPtr inicializován, nelze,
+** protože před první inicializací má ukazatel nedefinovanou (tedy libovolnou)
+** hodnotu. Programátor využívající ADT BVS tedy musí zajistit, aby inicializace
+** byla volána pouze jednou, a to před vlastní prací s BVS. Provedení
+** inicializace nad neprázdným stromem by totiž mohlo vést ke ztrátě přístupu
+** k dynamicky alokované paměti (tzv. "memory leak").
+**	
+** Všimněte si, že se v hlavičce objevuje typ ukazatel na ukazatel.	
+** Proto je třeba při přiřazení přes RootPtr použít dereferenční operátor *.
+** Ten bude použit i ve funkcích BSTDelete, BSTInsert a BSTDispose.
+**/
+
+    (*RootPtr) = NULL;
+
+}	
+
+int BSTSearch (tBSTNodePtr RootPtr, char K, int *Content)	{
+/*  ---------
+** Funkce vyhledá uzel v BVS s klíčem K.
+**
+** Pokud je takový nalezen, vrací funkce hodnotu TRUE a v proměnné Content se
+** vrací obsah příslušného uzlu.´Pokud příslušný uzel není nalezen, vrací funkce
+** hodnotu FALSE a obsah proměnné Content není definován (nic do ní proto
+** nepřiřazujte).
+**
+** Při vyhledávání v binárním stromu bychom typicky použili cyklus ukončený
+** testem dosažení listu nebo nalezení uzlu s klíčem K. V tomto případě ale
+** problém řešte rekurzivním volání této funkce, přičemž nedeklarujte žádnou
+** pomocnou funkci.
+**/
+
+    if (RootPtr == NULL) {
+        return FALSE;
+    }
+    else {
+        if (K < RootPtr->Key) {
+            BSTSearch(RootPtr->LPtr, K, Content);
+        } else if (K > RootPtr->Key) {
+            BSTSearch(RootPtr->RPtr, K, Content);
+        }
+        else {
+            *Content = RootPtr->BSTNodeCont;
+            return TRUE;
+        }
+    }
+} 
+
+
+void BSTInsert (tBSTNodePtr* RootPtr, char K, int Content)	{	
+/*   ---------
+** Vloží do stromu RootPtr hodnotu Content s klíčem K.
+**
+** Pokud již uzel se zadaným klíčem ve stromu existuje, bude obsah uzlu
+** s klíčem K nahrazen novou hodnotou. Pokud bude do stromu vložen nový
+** uzel, bude vložen vždy jako list stromu.
+**
+** Funkci implementujte rekurzivně. Nedeklarujte žádnou pomocnou funkci.
+**
+** Rekurzivní implementace je méně efektivní, protože se při každém
+** rekurzivním zanoření ukládá na zásobník obsah uzlu (zde integer).
+** Nerekurzivní varianta by v tomto případě byla efektivnější jak z hlediska
+** rychlosti, tak z hlediska paměťových nároků. Zde jde ale o školní
+** příklad, na kterém si chceme ukázat eleganci rekurzivního zápisu.
+**/
+
+    if ( RootPtr != NULL && (*RootPtr) != NULL) {
+        if ( K != (*RootPtr)->Key ) { // vyhledavani pokracuje v levem nebo pravem podstrumu
+            if ( K < ((*RootPtr)->Key) ) {
+                BSTInsert( &((*RootPtr)->LPtr), K, Content);
+            } else if ( K > (*RootPtr)->Key ) {
+                BSTInsert( &((*RootPtr)->RPtr), K, Content);
+            }
+        }
+        else { // aktualizace dat pri nalezeni stejneho klice
+            (*RootPtr)->BSTNodeCont = Content;
+        }
+    }
+    else {
+        // alokace pameti pro novy uzel
+        struct tBSTNode *newitem;
+        if ( (newitem = (struct tBSTNode*)malloc(sizeof(struct tBSTNode))) == NULL ) {
+            return;
+        }
+        // inicializace dat noveho uzlu
+        newitem->Key = K;
+        newitem->BSTNodeCont = Content;
+        newitem->LPtr = newitem->RPtr = NULL;
+
+        (*RootPtr) = newitem;
+    }
+
+}
+
+void ReplaceByRightmost (tBSTNodePtr PtrReplaced, tBSTNodePtr *RootPtr) {
+/*   ------------------
+** Pomocná funkce pro vyhledání, přesun a uvolnění nejpravějšího uzlu.
+**
+** Ukazatel PtrReplaced ukazuje na uzel, do kterého bude přesunuta hodnota
+** nejpravějšího uzlu v podstromu, který je určen ukazatelem RootPtr.
+** Předpokládá se, že hodnota ukazatele RootPtr nebude NULL (zajistěte to
+** testováním před volání této funkce). Tuto funkci implementujte rekurzivně. 
+**
+** Tato pomocná funkce bude použita dále. Než ji začnete implementovat,
+** přečtěte si komentář k funkci BSTDelete(). 
+**/
+
+    if ( (*RootPtr)->RPtr == NULL ) {
+        // prekopirovani hodnot uzlu
+        PtrReplaced->BSTNodeCont = (*RootPtr)->BSTNodeCont;
+        PtrReplaced->Key = (*RootPtr)->Key;
+        // uvolneni uzlu
+        BSTDelete(RootPtr, (*RootPtr)->Key);
+    }
+	else {
+        ReplaceByRightmost(PtrReplaced, &((*RootPtr)->RPtr));
+    }
+
+}
+
+void BSTDelete (tBSTNodePtr *RootPtr, char K) 		{
+/*   ---------
+** Zruší uzel stromu, který obsahuje klíč K.
+**
+** Pokud uzel se zadaným klíčem neexistuje, nedělá funkce nic. 
+** Pokud má rušený uzel jen jeden podstrom, pak jej zdědí otec rušeného uzlu.
+** Pokud má rušený uzel oba podstromy, pak je rušený uzel nahrazen nejpravějším
+** uzlem levého podstromu. Pozor! Nejpravější uzel nemusí být listem.
+**
+** Tuto funkci implementujte rekurzivně s využitím dříve deklarované
+** pomocné funkce ReplaceByRightmost.
+**/
+
+	if ( RootPtr && (*RootPtr) ) {
+        if ( K < (*RootPtr)->Key ) {
+            BSTDelete( &((*RootPtr)->LPtr), K);
+        }
+        else if ( K > (*RootPtr)->Key ) {
+            BSTDelete( &((*RootPtr)->RPtr), K);
+        }
+        else { // pokud byl nalezen uzel s danym klicem
+            if ( ((*RootPtr)->LPtr == NULL) && ((*RootPtr)->RPtr == NULL) ) { // pokud se jedna o listovy uzel
+                free(*RootPtr);
+                *RootPtr = NULL;
+            }
+            else if ( ((*RootPtr)->LPtr != NULL) && ((*RootPtr)->RPtr == NULL) ) { // pokud ma uzel jen levy podstrom
+                free(*RootPtr);
+                *RootPtr = (*RootPtr)->LPtr;
+            }
+            else if ( ((*RootPtr)->RPtr != NULL) && ((*RootPtr)->LPtr == NULL) ) { // pokud ma uzel jen pravy podstrom
+                free(*RootPtr);
+                *RootPtr = (*RootPtr)->RPtr;
+            }
+            else { // pokud ma ruseny uzel oba podstromy
+                ReplaceByRightmost((*RootPtr), &((*RootPtr)->LPtr));
+            }
+        }
+    }
+
+} 
+
+void BSTDispose (tBSTNodePtr *RootPtr) {	
+/*   ----------
+** Zruší celý binární vyhledávací strom a korektně uvolní paměť.
+**
+** Po zrušení se bude BVS nacházet ve stejném stavu, jako se nacházel po
+** inicializaci. Tuto funkci implementujte rekurzivně bez deklarování pomocné
+** funkce.
+**/
+	
+    if ( (*RootPtr) != NULL ) {
+        BSTDelete(RootPtr, (*RootPtr)->Key);
+        BSTDispose(RootPtr);
+    }
+
+}
+
+/* konec c401.c */
+
